ca8055708281e69cbff8e9a33ce2d4fc36b9347d
Jacob-Stump <jakedstump@gmail.com>
2023-10-28 22:26:04 -0500

    Display All Products from DB
    Essentially created a component that will display all Products listed in the database to learn Next.js/typescript. Somehow got this working don't ask me how

--- ecommerce-admin-portal/src/app/Dashboard.tsx
+++ ecommerce-admin-portal/src/app/Dashboard.tsx
@@ -1,33 +1,52 @@
-import React from 'react';
+'use client'
+import React, {useState, useEffect} from 'react';
 import { Container, Grid, Paper, Typography } from '@mui/material';
 
-const Dashboard: React.FC = () => {
-  return (
-    <Container>
-      <Typography variant="h1">Admin Dashboard</Typography>
-      <Grid container spacing={3}>
-        <Grid item xs={12} md={6} lg={4}>
-          <Paper>
-            <Typography variant="h6">Total Users</Typography>
-            <Typography variant="h4">500</Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <Paper>
-            <Typography variant="h6">Total Sales</Typography>
-            <Typography variant="h4">$2,500</Typography>
-          </Paper>
-        </Grid>
-        <Grid item xs={12} md={6} lg={4}>
-          <Paper>
-            <Typography variant="h6">Total Orders</Typography>
-            <Typography variant="h4">150</Typography>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Container>
-  );
-};
 
-export default Dashboard;
+const MyComponent = () => {
+  const [data, setData] = useState<Array<{product_name: string; descr: string; price: string; imgurl: string; quantity: number}>>([]);
+  
+  useEffect(() => {
+    //this effect will run when the component mounts
+    fetch('http://localhost:8080/inventory')
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result);
+      })
+      .catch((error) => {
+        console.error('Error fetching data');
+      });
+    }, []);
+
+    return (
+      <div>
+        <h2>Data Table</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Product Name</th>
+              <th>Description</th>
+              <th>Price</th>
+              <th>Image</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.product_name}</td>
+                <td>{item.descr}</td>
+                <td>{item.price}</td>
+                <td>{item.imgurl}</td>
+                <td>{item.quantity}S</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+  
+  export default MyComponent;
+ 
 
--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-//this small application seeks to create a new product within the MySQL database from a simple form on a webpage i.e. our website admin page.
-//most of these lines are comments, there's not much code just a lot to it.
-//we will go through this tracer program together, but if you're looking at it remember to start in the main and trace the code from there.
-//important to remember, this starts from the front end. This backend only listens to the endpoint(s) and retrieves the data that it has been "hit" with.
-//this means once the 'submit' button is clicked on the front end and the front end sends a request to the endpoint, this backend code will be listening to that endpoint.
-//This is partially why it is important to start on the front end.
-
 import ( //basically same as java imports
 
 	"database/sql"  //interface for querying/interacting with database
@@ -22,12 +15,15 @@ import ( //basically same as java imports
 
 type NewProduct struct { //essentially a java class, a struct is an object in Go and we are making a new product object called NewProduct
 
-	//each attribute within this object will be proportional to what we are requesting from the database or receiving from the front end
-	//since we are dealing with json for a web application, we use the `json: "X"` syntax in a struct to denote how the json will be marshaled (converted to json) or unmarshaled (parsed from json)
-	//in this case of SENDING information to this backend, we will be unmarshaling (or unwrapping/converting) the json and making an object out of the sent attributes.
-	//notice that we do not have a product_id attribute as we are CREATING a new product, and our database will auto increment a value so it is not needed for inserting.
-
 	//using the encoding/json package we imported, we can use the name declared "product_name" when unmarshaling the json. Same for the other attributes.
+	Name     string `json:"product_name"`
+	Desc     string `json:"descr"`
+	Price    string `json:"price"`
+	Img      string `json:"imgurl"`
+	Quantity int    `json:"quantity"`
+}
+
+type AllProducts struct {
 	Name     string `json:"product_name"`
 	Desc     string `json:"descr"`
 	Price    string `json:"price"`
@@ -61,8 +57,6 @@ func addProduct(w http.ResponseWriter, r *http.Request) { //method that will tak
 
 	defer db.Close()
 
-	//note that within the SQL query, there are ? ? ? ? ? for the values. That is because these values are the same values that were input on the front end form
-	//product_id MUST be declared, but requires a 0 or null for auto increment to function correctly, so it was not included in the struct
 	insertStatement, err := db.Prepare("INSERT INTO products (product_id, product_name, descr, price, imgurl, quantity) VALUES (?,?,?,?,?,?)") //creating prepared insert statement using databast/sql import package.
 
 	if err != nil {
@@ -79,12 +73,55 @@ func addProduct(w http.ResponseWriter, r *http.Request) { //method that will tak
 	}
 }
 
+func getProducts(w http.ResponseWriter, r *http.Request) {
+
+	cfg := mysql.Config{ //making our DB config to connect
+		User:   "root",
+		Passwd: "Frodobaggins123",
+		Net:    "tcp",
+		Addr:   "localhost:3306",
+		DBName: "golang",
+	}
+
+	db, err := sql.Open("mysql", cfg.FormatDSN()) //using DB config to connect
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	tuples, err := db.Query("SELECT product_name, descr, price, imgurl, quantity FROM products")
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer tuples.Close()
+
+	var products []AllProducts
+
+	for tuples.Next() { //for every row in products table..
+		var p AllProducts
+		err := tuples.Scan(&p.Name, &p.Desc, &p.Price, &p.Img, &p.Quantity)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		products = append(products, p)
+	}
+	//returning the product information as JSON
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
+
 func handleRequest(corsMiddleware func(http.Handler) http.Handler) { //method that utilizes mux import for handling multiple requests (we will have many requests for our project)
 	myRouter := mux.NewRouter().StrictSlash(true) //creating a new router instance to handle http requests
 	myRouter.Use(corsMiddleware)                  //telling our router to use the cors parameters we set
 
 	myRouter.HandleFunc("/addProduct", addProduct) //every time this https://localhost:8080/addProduct endpoint is hit from our front end code, we will utilize the addProduct function/method
-	//add more endpoints and associated funcs here
+	myRouter.HandleFunc("/inventory", getProducts)
 	//add more endpoints and associated funcs here
 	//add more endpoints and associated funcs here
 	//add more endpoints and associated funcs here
@@ -95,7 +132,7 @@ func main() {
 
 	//you cannot send HTTP if CORS is not enabled
 	corsMiddleware := handlers.CORS( //creates new CORS (Cross-Origin Resource Sharing) instance. Middleware for HTTP server allowing for specification on which origins, methods, and headers are allowed in cross-origin requests
-		handlers.AllowedOrigins([]string{"http://localhost:5500"}),                   // allowing which domains can send requests to the server, company's would have their website here
+		handlers.AllowedOrigins([]string{"http://localhost:3000"}),                   // allowing which domains can send requests to the server, company's would have their website here
 		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}), //allowing all http methods, for this initial program we will be using POST
 		handlers.AllowedHeaders([]string{"Content-Type"}),                            //specifying the header(s) allowed
 	)
